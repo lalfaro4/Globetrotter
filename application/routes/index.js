230728db8821b344b6c19e0320d408cecdc36655
Taylor Artunian <tartunian@gmail.com>
2021-06-07 22:14:03 -0700

    Added Ruja's M0 designs

--- application/routes/index.js
+++ application/routes/index.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var router = express.Router();
-// var database = require('../private/js/database');
-// var userIsLoggedIn = require('../middleware/routeProtectors').userIsLoggedIn;
 
 router.get('/', (req, res, next) => {
   res.render("welcome", {
@@ -9,40 +7,8 @@ router.get('/', (req, res, next) => {
   });
 });
 
-router.get('/about/:membername', (req, res, next) => {
-  res.render(`member-bios/${req.params.membername}`, {
-    layout: 'member-bio',
-    membername: `${req.params.membername}`
-  });
-});
-
-// router.get('/', userIsNotLoggedIn, (req, res, next) => {
-//   res.render("login", {
-//     title: "Login"
-//   });
-// });
-
-// router.get('/login', userIsNotLoggedIn, (req, res, next) => {
-//   res.render("login", {
-//     title: "Login"
-//   });
-// });
-
-// router.get('/registration', (req, res, next) => {
-//   res.render("registration", {
-//     title: "Registration"
-//   });
-// });
-
-// router.get('/home', userIsLoggedIn, getRecentPosts, (req, res, next) => {
-//   res.render("home", {
-//     title: "Home"
-//   });
-// });
-
 router.use((req, res) => {
   console.log(`index.js::errorTrap ${req.url}`);
-  // req.flash("error", "Couldn't find anything there.");
   res.redirect(404, "/");
 });
 

03d8a875f10853d47f9d8317d82d206fb7c151b1
Luis Alfaro <adri.alfaro.luis@gmail.com>
2021-07-17 16:41:42 -0700

    Add procedure to create a review with a registered_user, content, and service_provider. Add delete procedure to delete reviews. Add get to retreivce all the reviews.

--- database-setup/globetrotter-example-queries.sql
+++ database-setup/globetrotter-example-queries.sql
@@ -59,10 +59,46 @@ BEGIN
 END /
 DELIMITER ;
 
+# Create a review
+DELIMITER /
+DROP PROCEDURE IF EXISTS create_review/
+CREATE PROCEDURE create_review() 
+BEGIN
+	SELECT `user` INTO @owner_id FROM registered_user WHERE registered_user.username = 'globetrotter';
+	SELECT service_provider_id INTO @service_provider_id FROM service_provider WHERE service_provider.service_provider_name = 'Titan Airways';
+    CALL usp_create_review(@owner_id, 'Great flight!', @service_provider_id);
 
+	#CALL usp_create_activity(@owner_id, @trip_id, now(), date_add(now(), interval 7 day), 1, 1, NULL);
+END /
+DELIMITER ;
+
+DELIMITER /
+DROP PROCEDURE IF EXISTS delete_review/
+CREATE PROCEDURE delete_review()
+BEGIN
+	DELETE FROM review WHERE reviewer = '4f4e44f7-e730-11eb-bb81-28d24427a5d8';
+	#SELECT reviewer INTO @reviewer_id FROM review WHERE review.reviewer = '4f4e44f7-e730-11eb-bb81-28d24427a5d8';
+	DELETE FROM review WHERE review.review_id = @review_id;
+END /
+DELIMITER ;
+
+# Get all Reviews grouped by Service_Provider
+DELIMITER /
+DROP PROCEDURE IF EXISTS get_service_provider_reviews/
+CREATE PROCEDURE get_service_provider_reviews()
+BEGIN
+	    SELECT * FROM review;
+        #SELECT reviewer, service_provider FROM review;
+END /
+DELIMITER ;
 
 #CALL create_trip();
 #CALL delete_trip();
 #CALL get_trip_activities();
 #CALL get_profile_photo();
 CALL search_airline_name('merica');
+
+#CALL create_review();
+#CALL get_service_provider_reviews();
+#CALL delete_review();
+

aabc0c18a042105b2285c3192de51c9ab273c006
tartunian <tartunian@gmail.com>
2021-07-22 01:42:43 -0700

    Updated Handlebars navbar, some more layout issues on the Route Planner

---
+++ application/public/js/navbar.js
@@ -0,0 +1,6 @@
+
+document.getElementById("navbar-login-button").addEventListener('click', (event) => { window.location.href = '/public/html/login.html'});
+document.getElementById("navbar-signup-button").addEventListener('click', (event) => { window.location.href = '/public/html/registration.html'});
+document.getElementById("navbar-home-button").addEventListener('click', (event) => { window.location.href = '/public/html/home.html'});
+document.getElementById("navbar-aboutus-button").addEventListener('click', (event) => { window.location.href = '#'});
+document.getElementById("navbar-contact-button").addEventListener('click', (event) => { window.location.href = '#'});

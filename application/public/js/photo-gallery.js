c5100e500fd11a28f1d05bd94ceeff2ea16729fb
tartunian <tartunian@gmail.com>
2021-07-22 00:02:59 -0700

    Addes Jesus' files for M3

---
+++ application/public/js/photo-gallery.js
@@ -0,0 +1,36 @@
+const addButton = document.getElementById("add-img-btn");
+const grid = document.getElementById("images-container");
+
+// Everytime a user click the "+" button, the getRandomDogURL function gets called.
+addButton.addEventListener("click", getRandomDogURL);
+
+// This function gets called anytime the user hits the "+" button in the photo gallery. It displays an image of a puppy
+// for now. Will be made so that it will actually upload an image later on.
+function getRandomDogURL(){  
+    
+
+
+    const image = document.createElement("img");
+    
+    fetch('https://random.dog/woof.json')
+        .then(res => res.json())
+        .then(data => {
+            if(data.url.includes('.jpg') || data.url.includes('.mp4') || data.url.includes('.gif')){
+                getRandomDogURL();
+            }else{
+                image.src = data.url;
+                grid.appendChild(image);
+                    
+            }
+                    
+        })
+
+
+
+}
+
+// function createImage(event){
+   
+
+// }
+
---
+++ application/public/js/reset-password.js
@@ -0,0 +1,6 @@
+const sendButton = document.getElementById("send-button");
+
+// When the user clicks the "Send" button on the "Reset Password" page, a alert will be displayed to the user.
+sendButton.addEventListener("click", () => {
+    alert("Email to resest password has been sent to the provided email!")
+});

a2e983c260f209a4d8487d7710caceb8b39ff1ee
Jesus Correa <jcorrea2@mail.sfsu.edu>
2021-07-22 13:58:16 -0700

    Fixed an issue with the drop dpwn menu on the navigation bar. It would hide behind the background image. Also made fixed a small issue where the Save button in the photo gallery would not do anything. It now brings you back to the previous trips page.

--- application/public/js/photo-gallery.js
+++ application/public/js/photo-gallery.js
@@ -1,8 +1,11 @@
 const addButton = document.getElementById("add-img-btn");
 const grid = document.getElementById("images-container");
+const saveButton = document.getElementById("save-btn");
 
 // Everytime a user click the "+" button, the getRandomDogURL function gets called.
 addButton.addEventListener("click", getRandomDogURL);
+
+saveButton.addEventListener('click', (event) => { window.location.href = '/public/html/purchasedtrips.html'});
 
 // This function gets called anytime the user hits the "+" button in the photo gallery. It displays an image of a puppy
 // for now. Will be made so that it will actually upload an image later on.
